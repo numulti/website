70fcfd9f5c1ccfd6d5affa2513de0112efda6eab
daniaalnahdi <alnahdi.d@husky.neu.edu>
2021-01-03 08:21:52 +0300

    add temp homepage

--- src/components/temp-homepage/temp-homepage.jsx
+++ src/components/temp-homepage/temp-homepage.jsx
@@ -4,33 +4,32 @@ import { Container } from '@material-ui/core';
 import Schedule from '../schedule/schedule';
 import NewsletterForm from '../newsletter-form/newsletter-form';
 import SocialButton from '../social-button/social-button';
+import { LogoLight } from '../../assets/logos';
 import socials from '../../data/socials';
-
-const verticalAlign = {
-  height: '100vh',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-};
+import './temp-homepage.css';
 
 const TempHomePage = () => {
   return (
-    <div style={verticalAlign}>
+    <div className="v-align-container">
       <Container fixed>
-        <h1>MULTI</h1>
+        <section className="temp-home-logo">
+          <LogoLight />
+        </section>
         <p>
           MULTI is a student organization in Northeastern University advocating
           for diversity and inclusion in technology. It is founded under the
           Khoury College of Computer Sciences, and is based in Boston,
           Massachusetts.
         </p>
-        <div style={{ marginTop: '1em', marginBottom: '3em' }}>
-          <Schedule />
-        </div>
-        <NewsletterForm />
-        {socials.map(({ name, link, fa_icon }) => (
-          <SocialButton key={name} link={link} faIconName={fa_icon} />
-        ))}
+        <Schedule />
+        <section>
+          <NewsletterForm />
+        </section>
+        <section>
+          {socials.map(({ name, link, fa_icon }) => (
+            <SocialButton key={name} link={link} faIconName={fa_icon} />
+          ))}
+        </section>
       </Container>
     </div>
   );
