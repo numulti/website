dd80be157fb3af5dc1564777e3c871d48bd65a6f
Lavanya <lavanya.goel12@gmail.com>
2022-09-12 22:34:41 -0400

    Updated advisors and changed event for this week

--- src/data/events.js
+++ src/data/events.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 const events = [
   {
-    name: 'Fall \'22 Kickoff!',
+    name: "Fall '22 Kickoff!",
     description: (
       <p>
-        Celebrate fall semester with MULTI at our annual Fall kickoff!
-        Meet the E-board, talk to new friends, and learn more about what we do as a club!
-        Stick around for a fun game of Kahoot to win awesome prizes and eat some munchkins!
+        Celebrate fall semester with MULTI at our annual Fall kickoff! Meet the
+        E-board, talk to new friends, and learn more about what we do as a club!
+        Stick around for a fun game of Kahoot to win awesome prizes and eat some
+        munchkins!
       </p>
     ),
     series: 'Kickoff',
