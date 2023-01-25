b3a2098e207270e2cb9fe2b6aca582ffce7af308
Lavanya <lavanya.goel12@gmail.com>
2023-01-24 22:17:22 -0500

    Updated team and added events

--- src/data/events.js
+++ src/data/events.js
@@ -2,20 +2,41 @@ import React from 'react';
 
 const events = [
   {
-    name: "Spring '23 Kickoff",
+    name: "Day in the Life @ HubSpot",
     description: (
       <p>
-        Celebrate fall semester with MULTI at our annual Spring kickoff! Meet
-        the E-board, talk to new friends, and learn more about what we do as a
-        club!
-        <br></br>
-        Open to <b>undergrads only</b>
-        <br></br>
+        Join us this week to hear about the experiences of those 
+        working at HubSpot! Learn about the work software engineers 
+        and product managers do! They will have an open panel discussion, 
+        talk about diversity in the workplace, as well as list the current 
+        opportunities they have for Fall co-ops!
       </p>
     ),
-    series: 'General',
-    day: '18',
+    series: 'Tech Talk',
+    day: '25',
     month: 'January',
+    year: '2023',
+    time: {
+      start: '7:00PM',
+      end: '8:00PM',
+    },
+    location: {
+      name: '144 Curry Student Center (CIE)',
+    },
+    cancelled: false,
+  },
+  {
+    name: "Preparing for Co-Ops Panel",
+    description: (
+      <p>
+        Listen and ask questions to current students who have already 
+        gone on their first or second co-op about their experiences and 
+        skills they learned while on co-op!
+      </p>
+    ),
+    series: 'Tech Talk',
+    day: '01',
+    month: 'February',
     year: '2023',
     time: {
       start: '7:00PM',
