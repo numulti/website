700bca55ad27a34f5d62929cbb89e8e205f3a1d8
Lavanya <lavanya.goel12@gmail.com>
2023-06-13 09:54:59 -0400

    Updated team and events summer '23

--- src/data/events.js
+++ src/data/events.js
@@ -2,59 +2,15 @@ import React from 'react';
 
 const events = [
   {
-    name: 'Class Registration & Info Session',
+    name: 'TBD',
     description: (
       <p>
-        Currently planning a schedule for the Fall semester? Join us for an 
-        info session on class registration and co-ops! We'll be giving advice 
-        on academic planning, common Khoury classes, and applying for co-op!
+        We'll be back in the fall semester, see you then!
       </p>
     ),
     series: 'Coding 404',
-    day: '05',
-    month: 'April',
-    year: '2023',
-    time: {
-      start: '7:00PM',
-      end: '8:00PM',
-    },
-    location: {
-      name: '144 Curry Student Center (CIE)',
-    },
-    cancelled: false,
-  },
-  {
-    name: 'Ethics with Professor Ricks',
-    description: (
-      <p>
-        Professor Ricks will be speaking to us about machine learning and the 
-        ethics of AI and ChatGPT. We will also have cookies!
-      </p>
-    ),
-    series: 'Breaking the Stigma',
-    day: '12',
-    month: 'April',
-    year: '2023',
-    time: {
-      start: '7:00PM',
-      end: '8:00PM',
-    },
-    location: {
-      name: '144 Curry Student Center (CIE)',
-    },
-    cancelled: false,
-  },
-  {
-    name: 'MULTI Picnic',
-    description: (
-      <p>
-        Join us for our last meeting of the semester, where we will celebrate 
-        with a picnic/potluck!
-      </p>
-    ),
-    series: 'Coding 404',
-    day: '19',
-    month: 'April',
+    day: '6',
+    month: 'September',
     year: '2023',
     time: {
       start: '7:00PM',
