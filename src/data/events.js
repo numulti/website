6d7206611ae65eae833538efdb90fbf3453789ca
DamianUduevbo <58115973+DamianUduevbo@users.noreply.github.com>
2023-09-24 21:44:59 -0400

    updated events calendar (NER x MULTI and Intro to App Dev)

--- src/data/events.js
+++ src/data/events.js
@@ -3,11 +3,7 @@ import React from 'react';
 const events = [
   {
     name: 'TBD',
-    description: (
-      <p>
-        We'll be back in the fall semester, see you then!
-      </p>
-    ),
+    description: <p>We'll be back in the fall semester, see you then!</p>,
     series: 'Coding 404',
     day: '6',
     month: 'September',
@@ -25,7 +21,10 @@ const events = [
     name: 'NER x MULTI Mixer',
     description: (
       <p>
-        Join us for an exciting social mixer with Northeastern Electric Racing, where you can look forward to a fun-filled evening of activities, such as Human Bingo and more, and delicious catering. A perfect opportunity to make new friends and connections!
+        Join us for an exciting social mixer with Northeastern Electric Racing,
+        where you can look forward to a fun-filled evening of activities, such
+        as Human Bingo and more, and delicious catering. A perfect opportunity
+        to make new friends and connections!
       </p>
     ),
     series: '',
@@ -45,7 +44,10 @@ const events = [
     name: 'Intro to App Development',
     description: (
       <p>
-        Want to learn App Development? Join us for an engaging workshop where you can dive into the world of app development and turn your creative app ideas into reality. Don't miss out on this opportunity to level up your coding expertise!
+        Want to learn App Development? Join us for an engaging workshop where
+        you can dive into the world of app development and turn your creative
+        app ideas into reality. Don't miss out on this opportunity to level up
+        your coding expertise!
       </p>
     ),
     series: 'Codign 404',
