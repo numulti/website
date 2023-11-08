f54b959ff384c89225f863e00ede444f97c42cdf
DamianUduevbo <58115973+DamianUduevbo@users.noreply.github.com>
2023-11-08 13:23:10 -0500

    updated calendar

--- src/data/events.js
+++ src/data/events.js
@@ -2,34 +2,16 @@ import React from 'react';
 
 const events = [
   {
-    name: 'TBD',
-    description: <p>We'll be back in the fall semester, see you then!</p>,
-    series: 'Coding 404',
-    day: '6',
-    month: 'September',
-    year: '2023',
-    time: {
-      start: '7:00PM',
-      end: '8:00PM',
-    },
-    location: {
-      name: '144 Curry Student Center (CIE)',
-    },
-    cancelled: false,
-  },
-  {
-    name: 'NER x MULTI Mixer',
+    name: 'MULTI x CarGurus: Tech Talk',
     description: (
       <p>
-        Join us for an exciting social mixer with Northeastern Electric Racing,
-        where you can look forward to a fun-filled evening of activities, such
-        as Human Bingo and more, and delicious catering. A perfect opportunity
-        to make new friends and connections!
+        Are you interested in working at CarGurus, or are you just curious about how to approach interviews?
+        Join us for a tech talk with CarGurus, where developers and recuiters will be sharing information about the company also giving advice on how to approach interviews.
       </p>
     ),
-    series: '',
-    day: '27',
-    month: 'September',
+    series: 'Tech Talk',
+    day: '25',
+    month: 'October',
     year: '2023',
     time: {
       start: '7:00PM',
@@ -41,18 +23,39 @@ const events = [
     cancelled: false,
   },
   {
-    name: 'Intro to App Development',
+    name: 'MULTI x Oasis: Intro to App Development',
     description: (
       <p>
-        Want to learn App Development? Join us for an engaging workshop where
+        Want to learn App Development? Join MULTI and Oasis for an engaging workshop where
         you can dive into the world of app development and turn your creative
         app ideas into reality. Don't miss out on this opportunity to level up
         your coding expertise!
       </p>
     ),
-    series: 'Codign 404',
-    day: '11',
+    series: 'Coding 404',
+    day: '25',
     month: 'October',
+    year: '2023',
+    time: {
+      start: '7:00PM',
+      end: '8:00PM',
+    },
+    location: {
+      name: '144 Curry Student Center (CIE)',
+    },
+    cancelled: false,
+  },
+  {
+    name: 'Interview and Resume Workshop',
+    description: (
+      <p>
+        Are you applying to co-ops or internships? Join us on Wednesday to receive advice
+        about how to tackle behavioural interviews! We will also be doing resume reviews, so feel free to bring your resume!
+      </p>
+    ),
+    series: '',
+    day: '1',
+    month: 'November',
     year: '2023',
     time: {
       start: '7:00PM',
@@ -67,8 +70,10 @@ const events = [
     name: 'C4C x MULTI: Intro to Personal Projects',
     description: (
       <p>
-        Join Code4Community and MULTI for our personal projects workshop, where we will go over the fundamentals of building personal projects for resumes.
-        We will go over example projects, brainstorming ideas, typical tech stacks, and how to get started.
+        Join Code4Community and MULTI for our personal projects workshop, where
+        we will go over the fundamentals of building personal projects for
+        resumes. We will go over example projects, brainstorming ideas, typical
+        tech stacks, and how to get started.
       </p>
     ),
     series: 'LEVEL-UP',
